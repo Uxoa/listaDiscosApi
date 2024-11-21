fdff8aabd41dd14730cb34519095bdaa9b3a312f
uxoa <palomita@gmail.com>
2024-11-21 22:04:51 +0100

    Feat: DiscoController

---
+++ src/main/java/org/factoriaF5/DiscoControlador.java
@@ -0,0 +1,20 @@
+package org.factoriaF5;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/discos")
+public class DiscoControlador {
+    
+   @Autowired
+   public DiscoRepositorio discoRepositorio;
+   
+   @PostMapping
+   public Disco crearDisco(@RequestBody Disco disco){
+       this.discoRepositorio.save(disco);
+       return disco;
+   }
+
+
+}
--- src/main/java/org/factoriaF5/DiscoRepositorio.java
+++ src/main/java/org/factoriaF5/DiscoRepositorio.java
@@ -1,12 +1,8 @@
 package org.factoriaF5;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 public interface DiscoRepositorio extends JpaRepository{
-
-
 
 }
