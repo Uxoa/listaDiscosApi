c90bd044803d5bc0ea1ceb6f08d78d450a9ef9f3
uxoa <palomita@gmail.com>
2024-11-21 21:42:06 +0100

    Feat: DiscoRepository

--- src/main/java/org/factoriaF5/DiscoRepositorio.java
+++ src/main/java/org/factoriaF5/DiscoRepositorio.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-public interface DiscoRepositorio{
+public interface DiscoRepositorio extends JpaRepository{
 
 
 
