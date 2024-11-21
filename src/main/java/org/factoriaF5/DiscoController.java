1d9c2673db4c0fd8b3f099d55217319724273603
uxoa <palomita@gmail.com>
2024-11-21 21:41:25 +0100

    Feat: DiscoRepository

---
+++ src/main/java/org/factoriaF5/DiscoController.java
@@ -0,0 +1,11 @@
+package org.factoriaF5;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/discos")
+public class DiscoController {
+
+
+}
